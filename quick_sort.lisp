b2c98e2e40fbfb32e440259462e8dd8d53f09246
OwOverlord <lucaslej19@gmail.com>
2020-12-30 18:06:51 +0100

    modifié :         quick_sort.lisp

--- quick_sort.lisp
+++ quick_sort.lisp
@@ -1,15 +1,15 @@
 (defun quick-sort (lat)
   "Implémentation récursive de l'algorithme de tri-rapide."
   (if lat
-    (let ((partition (car lat))
-          (rest (cdr lat)))
-      (append
+      (let ((partition (car lat))
+            (rest (cdr lat)))
+      (nconc
        (quick-sort (remove-if (lambda (a)
                                 (>= a partition))
                               rest))
-       (cons partition
-             (quick-sort (remove-if (lambda (a)
-                          (< a partition))
-                        rest)))))
+       (remove-if (lambda (a) (/= a partition)) lat)
+       (quick-sort (remove-if (lambda (a)
+                                (<= a partition))
+                              rest))))
     nil))
 
