7650a9e83a78da12a54ce0c9a1adfa48660a3d6c
OwOverlord <lucaslej19@gmail.com>
2021-01-05 19:57:09 +0100

    Add a playing repl and a playing function.

--- Connect4/connect4-game.lisp
+++ Connect4/connect4-game.lisp
@@ -22,3 +22,38 @@
                     (setf board (map 'vector #'cdr board)))
             finally (format t "~{~D ~}~%"
                             (count-to 7)))))
+
+(defun fullp (i board)
+  (not (car (aref board i))))
+
+(defun tiedp (board)
+  (notany #'null
+          (map 'list #'car board)))
+
+(defun play (i color board)
+  (flet ((push-until (lat token)
+           (when (every #'null lat)
+             (setf (sixth lat) token)
+             lat)
+           (cond
+             ((cadr lat) (cons token (cdr lat)))
+             (t (cons (car lat)
+                      (push-until (cdr lat) token))))))
+    (push-until (aref board i) color)))
+
+(defun play-repl ()
+  (loop while (and (not (winningp *board*))
+                   (not (tiedp *board*)))
+        do (progn
+             (format t "A votre tour!~%Dans quelle colonne souhaitez-vous jouer? ")
+             (let* ((column (read))
+                    (i (1- column)))
+               (cond
+                 ((not (<= 1 column 7))
+                  (format t "Veuillez jouer un numéro de colonne correct!~%")
+                  (play-repl))
+                 ((fullp i *board*)
+                  (format t "Cette colonne est déjà remplie!~%")
+                  (play-repl))
+                 (t (setf *board* (play i 'yellow *board*)))))
+             (setf *board* (computer-turn 'red *board*)))))
