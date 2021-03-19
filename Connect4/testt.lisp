2ca3955affd5f765f8daf5d2fb586de2266b8e74
OwOverlord <lucaslej19@gmail.com>
2021-03-19 15:33:01 +0100

    Making more sense with our files.

--- Connect4/testt.lisp
+++
@@ -1,9 +0,0 @@
-(defun weird-factorial (a &optional (buffer 1))
-  (flet ((writer (x) (setf buffer (* buffer x))))
-    (%weird-factorial a #'writer))
-  buffer)
-
-(defun %weird-factorial (a writer)
-  (unless (= a 0)
-    (funcall writer a)
-    (%weird-factorial (1- a) writer)))
--- Connect4/this-is-a-test.lisp
+++
@@ -1,7 +0,0 @@
-(defparameter *buffer* 1)
-(defun weird-factorial (a)
-  (unless (= a 0)
-    (progn
-      (setf *buffer* (* a *buffer*))
-      (weird-factorial (1- a))))
-  *buffer*)
